0b3458675735dbfc12b165fa5072df8f488f9447
Kovalenko Makcim <max.kow@yandex.ru>
2021-06-12 20:06:19 +0300

    дз 11
    Выполнена логика автоматической смены цвета body каждую 1с.

---
+++ index.js
@@ -0,0 +1,40 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+const refs = {
+	btnStart: document.querySelector('[data-action="start"]'),
+	btnStop: document.querySelector('[data-action="stop"]'),
+	body: document.querySelector('body')
+};
+
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', onChangeBodyColor);
+refs.btnStop.addEventListener('click', onStopChangeBodyColor);
+
+
+function onChangeBodyColor() {
+	if (intervalId) {
+		return;
+	};
+
+	intervalId = setInterval(() => {
+		const randomIntegerFromInterval = (min, max) => {
+			return Math.floor(Math.random() * (max - min + 1) + min);
+		};
+		refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+	}, 1000);
+}
+
+function onStopChangeBodyColor() {
+	clearInterval(intervalId);
+	intervalId = null;
+}
+
